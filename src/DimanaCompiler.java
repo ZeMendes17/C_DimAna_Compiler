034143b53bee2c5744d5b7c832a226ba32ee5e5e
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-05-10 10:41:50 +0100

    start/creation of visitor and stg file

---
+++ src/DimanaCompiler.java
@@ -0,0 +1,162 @@
+
+import java.util.*;
+import org.stringtemplate.v4.*;
+@SuppressWarnings("CheckReturnValue")
+public class DimanaCompiler extends dimanaBaseVisitor<String> {
+
+   @Override public String visitProgram(dimanaParser.ProgramContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitStatList(dimanaParser.StatListContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitStatement(dimanaParser.StatementContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitVariableDeclaration(dimanaParser.VariableDeclarationContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitAssignment(dimanaParser.AssignmentContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitOutputStatement(dimanaParser.OutputStatementContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitLoopStatement(dimanaParser.LoopStatementContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitHeaderFile(dimanaParser.HeaderFileContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitPrefixUnit(dimanaParser.PrefixUnitContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitUnit(dimanaParser.UnitContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitAlternativeUnit(dimanaParser.AlternativeUnitContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitListDeclaration(dimanaParser.ListDeclarationContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitExprListExpression(dimanaParser.ExprListExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitIndexExpression(dimanaParser.IndexExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitInputExpression(dimanaParser.InputExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitAddSubExpression(dimanaParser.AddSubExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitRealLiteral(dimanaParser.RealLiteralContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitStringAssignExpression(dimanaParser.StringAssignExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitTypeConversion(dimanaParser.TypeConversionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitStringLiteral(dimanaParser.StringLiteralContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitAddListExpression(dimanaParser.AddListExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitIdExpression(dimanaParser.IdExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitParenExpression(dimanaParser.ParenExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitIntLiteral(dimanaParser.IntLiteralContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitMulDivExpression(dimanaParser.MulDivExpressionContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+
+   @Override public String visitDataType(dimanaParser.DataTypeContext ctx) {
+      String res = null;
+      return visitChildren(ctx);
+      //return res;
+   }
+}
---
+++ src/dimanaBaseListener.java
@@ -0,0 +1,351 @@
+// Generated from dimana.g4 by ANTLR 4.12.0
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link dimanaListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class dimanaBaseListener implements dimanaListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(dimanaParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(dimanaParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatList(dimanaParser.StatListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatList(dimanaParser.StatListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(dimanaParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(dimanaParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariableDeclaration(dimanaParser.VariableDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariableDeclaration(dimanaParser.VariableDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment(dimanaParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment(dimanaParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOutputStatement(dimanaParser.OutputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOutputStatement(dimanaParser.OutputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLoopStatement(dimanaParser.LoopStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLoopStatement(dimanaParser.LoopStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterHeaderFile(dimanaParser.HeaderFileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitHeaderFile(dimanaParser.HeaderFileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrefixUnit(dimanaParser.PrefixUnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrefixUnit(dimanaParser.PrefixUnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUnit(dimanaParser.UnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUnit(dimanaParser.UnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAlternativeUnit(dimanaParser.AlternativeUnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAlternativeUnit(dimanaParser.AlternativeUnitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterListDeclaration(dimanaParser.ListDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitListDeclaration(dimanaParser.ListDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExprListExpression(dimanaParser.ExprListExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExprListExpression(dimanaParser.ExprListExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIndexExpression(dimanaParser.IndexExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIndexExpression(dimanaParser.IndexExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputExpression(dimanaParser.InputExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputExpression(dimanaParser.InputExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAddSubExpression(dimanaParser.AddSubExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAddSubExpression(dimanaParser.AddSubExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRealLiteral(dimanaParser.RealLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRealLiteral(dimanaParser.RealLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStringAssignExpression(dimanaParser.StringAssignExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStringAssignExpression(dimanaParser.StringAssignExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTypeConversion(dimanaParser.TypeConversionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTypeConversion(dimanaParser.TypeConversionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStringLiteral(dimanaParser.StringLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStringLiteral(dimanaParser.StringLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAddListExpression(dimanaParser.AddListExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAddListExpression(dimanaParser.AddListExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIdExpression(dimanaParser.IdExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIdExpression(dimanaParser.IdExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParenExpression(dimanaParser.ParenExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParenExpression(dimanaParser.ParenExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIntLiteral(dimanaParser.IntLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIntLiteral(dimanaParser.IntLiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMulDivExpression(dimanaParser.MulDivExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMulDivExpression(dimanaParser.MulDivExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDataType(dimanaParser.DataTypeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDataType(dimanaParser.DataTypeContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
