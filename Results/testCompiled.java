f814afc4137cccf7c682a90bf784927f0cf26364
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-06-03 15:47:31 +0100

    more work in loops
    Co-authored-by: André Oliveira <andreaoliveira@ua.pt>

---
+++ Results/testCompiled.java
@@ -0,0 +1,42 @@
+import java.util.*;
+//import java.io.*;
+
+public class testCompiled {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); /*para depois usar nos READS*/
+      DimensionVar x = new DimensionVar("Length");
+      x.setSuffix("m");
+      DimensionVar y = new DimensionVar("Length");
+      y.setSuffix("m");
+      double v1;
+      v1 = 1.0;
+      x.setValue_double(v1);
+      double v2;
+      v2 = 2.0;
+      y.setValue_double(v2);
+      int N = 4;
+      int z = 0;
+      int v3 = N;
+
+      int v4 = 6;
+      do {
+         System.out.print(N);
+         continue;
+
+      } while (
+      v3<v4);
+      int v5 = N;
+
+      int v6 = 5;
+      if (
+      v5<v6) {
+         int v7 = 300;
+         N = v7;
+
+      } else {
+      }
+      sc.close(); /*tem de fechar*/
+
+   }
+}
